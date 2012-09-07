137fd3d0e0a74e274530ccf432e8f77bd6465fbe
Stephen Cleary <git.ourteddybear@xoxy.net>
2012-09-07 19:54:41 -0400

    Changed namespace from MvcApplication1 to StephenCleary.

--- StephenCleary.com/Controllers/HomeController.cs
+++ StephenCleary.com/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace MvcApplication1.Controllers
+namespace StephenCleary.Controllers
 {
     public class HomeController : Controller
     {
