00baa7b08e545d9bb9c7df49c528369dfdc946a9
Stephen Cleary <git.ourteddybear@xoxy.net>
2012-09-08 23:25:21 -0400

    Changed Site.css to relative addressing.

--- StephenCleary.com/App_Start/BundleConfig.cs
+++ StephenCleary.com/App_Start/BundleConfig.cs
@@ -8,8 +8,6 @@ namespace StephenCleary
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/site.css"));
         }
     }
 }
