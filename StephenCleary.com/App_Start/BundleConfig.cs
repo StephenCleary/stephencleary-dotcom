50a33950428344088dd8995c3d5538f2eb985aed
Stephen Cleary <git.ourteddybear@xoxy.net>
2012-09-08 20:10:57 -0400

    Incorporated Init.js into main page, and changed img tags to load after page initialization.

--- StephenCleary.com/App_Start/BundleConfig.cs
+++ StephenCleary.com/App_Start/BundleConfig.cs
@@ -8,9 +8,6 @@ namespace StephenCleary
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/Init").Include(
-                        "~/Scripts/Init-{version}.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/site.css"));
         }
--- StephenCleary.com/Scripts/Init-1.0.0.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).bind("mobileinit", function () {
-    $.mobile.defaultPageTransition = 'none';
-    
-    var x = 'an';
-    var y = 'steph';
-    $('#email').attr({ href: 'mailto:' + y + 'en' + x + 'dm' + x + 'dy@' + 'gma' + 'il.' + 'com' });
-
-    $('#main').removeClass('hidden');
-});
